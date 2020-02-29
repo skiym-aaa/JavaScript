70a5117a8f79aa5b4a194bae759084f2539ed09c
skiym-aaa <y.sakiyama3216@gmail.com>
2020-02-29 18:10:05 +0900

    [Finish] 6-8

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -1,3 +1,5 @@
-$(document).ready(function(){
-  $('body').html('<h1>Hello jQuery!!</h1>')
-})
+$(function(){
+  $('button').on('click',function(){
+    $('ul').children().css('color','red');
+  })
+});
